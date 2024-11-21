f9c20d7adaac984a2369e1b992c4fece1b35abf7
M0sT4fa <mostafa2018a3s@gmail.com>
2024-11-21 11:22:14 +0200

    feat(icon): add app and executable icons

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 
 #include <QApplication>
 #include <iostream>
-#include "docreaderquizes.h"
+#include "docreaderquizzes.h"
 #include "fileparser.h"
+#include <QIcon>
 
 int main(int argc, char** argv) {
 
@@ -10,7 +11,16 @@ int main(int argc, char** argv) {
 	app.setOrganizationName("DocReader");
 	app.setApplicationName("Quizzes");
 
+	QIcon appIcon{ "./assets/icons/appicon.ico" };
+
+	if (appIcon.isNull())
+		qInfo() << "App icon didn't load.";
+	else
+		app.setWindowIcon(appIcon);
+
 	DocReaderQuizzes mainApp{};
+
+	//mainApp.setWindowIcon(QIcon{ ":/icons/appicon.ico" });
 	mainApp.show();
 
 	app.exec();
